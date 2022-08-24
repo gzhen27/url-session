723d9dd86098547c52b16840ac064f63f2ebab58
Gray <grayzprogrammer@gmail.com>
2022-08-23 20:17:27 -0500

    downloads an image via URLSession.dataTask

---
+++ NetworkingPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    ///extends as needed
+}
+
+let imageUrl = URL(string: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg")!
+
+var request = URLRequest(url: imageUrl)
+request.httpMethod = HTTPMethod.get.rawValue
+
+let imageTask = URLSession.shared.dataTask(with: imageUrl) { imageData, res, err in
+    if let err = err {
+        print("Not able to fetch the image data: \(err.localizedDescription)")
+        return
+    }
+    
+    guard let res = res as? HTTPURLResponse, (200...299).contains(res.statusCode) else {
+        print("Server Error: Please try again later!")
+        return
+    }
+    
+    if let data = imageData, let image = UIImage(data: data) {
+        print("Fetched the image successfully!")
+        let cat = image
+        print(cat)
+    }
+}
+
+imageTask.resume()

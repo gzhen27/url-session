32f4aacd9c16d39a7e3b6e8eec5aeba9285cec87
Gray <grayzprogrammer@gmail.com>
2022-08-25 20:47:52 -0500

    adds the image info

--- NetworkingPlayground.playground/Pages/CatBreeds.xcplaygroundpage/Contents.swift
+++ NetworkingPlayground.playground/Pages/CatBreeds.xcplaygroundpage/Contents.swift
@@ -5,12 +5,20 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+struct BreedImage: Codable {
+    let id: String
+    let url: String
+    let width: Int
+    let height: Int
+}
+
 struct CatBreed: Codable, CustomStringConvertible {
     let id: String
     let name: String
     let temperament: String
     let lifeSpan: String
     let isHairless: Bool
+    let image: BreedImage
     
     // need to call init to assgin the value properly because hairless is an Int from the CatAPI, but we want it to be Bool.
     init(from decoder: Decoder) throws {
@@ -20,26 +28,31 @@ struct CatBreed: Codable, CustomStringConvertible {
         name = try values.decode(String.self, forKey: .name)
         temperament = try values.decode(String.self, forKey: .temperament)
         lifeSpan = try values.decode(String.self, forKey: .lifeSpan)
+        image = try values.decode(BreedImage.self, forKey: .image)
         
         let hairless = try values.decode(Int.self, forKey: .isHairless)
         isHairless = hairless == 1
     }
     
     enum CodingKeys: String, CodingKey {
-        case id, name, temperament
+        case id, name, temperament, image
         case lifeSpan = "life_span"
         case isHairless = "hairless"
     }
 
     var description: String {
         return """
-        -----------Cat Info-------------
+        \n
+        >--------------Cat Info----------------<
+        \n
         id: \(id)
         name: \(name)
         termperament: \(temperament)
         life span: \(lifeSpan)
         is hairlesss: \(isHairless)
-        --------------------------------
+        imageURL: \(image.url)
+        \n
+        >--------------------------------------<
         """
     }
 }
